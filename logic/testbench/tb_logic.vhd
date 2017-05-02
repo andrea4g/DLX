03a02f66ca01882a2ed790a428a965f7ad88fcfc
Andrea <andrea.gualco@gmail.com>
2017-05-02 12:02:36 +0200

    logic working correctly

--- logic/testbench/tb_logic.vhd
+++ logic/testbench/tb_logic.vhd
@@ -27,18 +27,20 @@ begin
   tb_logic_input_process : process
   begin
 
-    tb_r1 <= '0', '1' after 30 ns, '0' after 60 ns, '1' after 90 ns;
-    tb_r2 <= '0', '0' after 30 ns, '1' after 60 ns, '1' after 90 ns;
+    tb_r1 <= '0', '1' after 20 ns, '0' after 40 ns, '1' after 60 ns;
+    tb_r2 <= '0', '0' after 20 ns, '1' after 40 ns, '1' after 60 ns;
+    wait for 70 ns;
 
   end process;
 
   tb_logic_select_process : process
   begin
     --       AND  NAND            OR               NOR              XOR             XNOR
-    tb_s0 <= '0', '1' after 5 ns, '0' after 10 ns, '1' after 15 ns, '0' after 20 ns, '1' after 25 ns;
-    tb_s1 <= '0', '1' after 5 ns, '1' after 10 ns, '0' after 15 ns, '1' after 20 ns, '0' after 25 ns;
-    tb_s2 <= '0', '1' after 5 ns, '1' after 10 ns, '0' after 15 ns, '1' after 20 ns, '0' after 25 ns;
-    tb_s3 <= '1', '0' after 5 ns, '1' after 10 ns, '0' after 15 ns, '0' after 20 ns, '1' after 25 ns;
+    tb_s0 <= '0', '1' after 2 ns, '0' after 4 ns, '1' after 6 ns, '0' after 8 ns, '1' after 10 ns;
+    tb_s1 <= '0', '1' after 2 ns, '1' after 4 ns, '0' after 6 ns, '1' after 8 ns, '0' after 10 ns;
+    tb_s2 <= '0', '1' after 2 ns, '1' after 4 ns, '0' after 6 ns, '1' after 8 ns, '0' after 10 ns;
+    tb_s3 <= '1', '0' after 2 ns, '1' after 4 ns, '0' after 6 ns, '0' after 8 ns, '1' after 10 ns;
+    wait for 20 ns;
   end process;
 
 end architecture;
