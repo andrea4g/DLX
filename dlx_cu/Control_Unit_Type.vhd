9bec51d79e280b79042c61bb452c8949469ac138
Pietro Inglese <magicenglish93@gmail.com>
2017-05-31 00:04:59 +0200

    add of types, must do the comments

--- dlx_cu/Control_Unit_Type.vhd
+++ dlx_cu/Control_Unit_Type.vhd
@@ -1,3 +1,4 @@
+-- ToDo: comments
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -7,7 +8,7 @@ package Control_Unit_Type is
   constant FUNC_SIZE       : integer :=  11;                                      -- FUNC field size
 
   -- recall that RD is always the address of the output
-  
+
   -- R-Type instruction -> OPCODE field
   constant NOP     : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "000000";    -- no operation
   constant RTYPE   : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "000001";    -- for ADD, SUB, AND, OR register-to-register operation
@@ -16,6 +17,13 @@ package Control_Unit_Type is
   constant R_SUB : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000000001";    -- SUB RS1,RS2,RD
   constant R_AND : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000000010";    -- AND RS1,RS2,RD
   constant R_OR  : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000000011";    -- OR  RS1,RS2,RD
+  constant R_SGE : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000000100";    -- OR  RS1,RS2,RD
+  constant R_SLE : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000000101";    -- OR  RS1,RS2,RD
+  constant R_SLL : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000000110";    -- OR  RS1,RS2,RD
+  constant R_SNE : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000000111";    -- OR  RS1,RS2,RD
+  constant R_SRL : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000001000";    -- OR  RS1,RS2,RD
+  constant R_SUB : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000001001";    -- OR  RS1,RS2,RD
+  constant R_XOR : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000001010";    -- OR  RS1,RS2,RD
 
   -- I-Type instruction -> OPCODE field
   constant I_ADDI1 : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "000010";    -- ADDI1  RS1,RD,INP1
@@ -32,6 +40,20 @@ package Control_Unit_Type is
   constant S_MEM1  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "001101";    -- S MEM2 RS1,RS2,INP1 -- MEM[R[RS1]+INP2] = R[RS2]
   constant S_MEM2  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "001100";    -- S_MEM2 RS1,RD, INP2
   constant L_MEM1  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "001111";    -- L_MEM1 RS1,RS2,INP1 -- R[RS2] = MEM[R[RS1]+INP1]
-  constant S_MEM2  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010000";    -- L_MEM2 RS1,RS2,INP2
+  constant S_MEM2  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010010";    -- L_MEM2 RS1,RS2,INP2
+  constant I_SGEI  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010001";    -- L_MEM2 RS1,RS2,INP2
+  constant I_SLEI  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010000";    -- L_MEM2 RS1,RS2,INP2
+  constant I_SLLI  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010011";    -- L_MEM2 RS1,RS2,INP2
+  constant I_SNEI  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010110";    -- L_MEM2 RS1,RS2,INP2
+  constant I_SRLI  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010101";    -- L_MEM2 RS1,RS2,INP2
+  constant I_SUBI  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010100";    -- L_MEM2 RS1,RS2,INP2
+  constant I_XORI  : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "010111";    -- L_MEM2 RS1,RS2,INP2
 
+  -- J-Type instruction -> OPCODE field
+  constant BEQZ    : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "011011";    -- L_MEM2 RS1,RS2,INP2
+  constant BNEZ    : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "011000";    -- L_MEM2 RS1,RS2,INP2
+  constant J       : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "011001";    -- L_MEM2 RS1,RS2,INP2
+  constant JAL     : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "011010";    -- L_MEM2 RS1,RS2,INP2
+  constant LW      : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "011111";    -- L_MEM2 RS1,RS2,INP2
+  constant NOP     : std_logic_vector(OP_CODE_SIZE - 1 downto 0) := "011100";    -- L_MEM2 RS1,RS2,INP2
 end package;
