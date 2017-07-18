fee9a488fc4aedd10ccd1535ff0957035d836f39
Pietro <magicenglish93@gmail.com>
2017-07-18 12:59:18 +0200

    modification to tcl file, move of files to perform synthesis

--- syn/dlx.tcl
+++ syn/dlx.tcl
@@ -2,7 +2,11 @@ set LIBRARY work
 set SOURCE_DIRECTORY ../src
 set DEST_DIRECTORY report
 set SOURCE_FILES {
+  000-functions.vhd
   000-globals.vhd
+  000-rocache.vhd
+  000-rwcache.vhd
+  a-DLX.vhd
   a.a-cu.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a-basic_blocks/a.c.a.a-and_2.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a-basic_blocks/a.c.a.a-and_3.vhd
@@ -17,10 +21,10 @@ set SOURCE_FILES {
   a.b-DataPath.core/a.b-alu.core/a.c.a-basic_blocks/a.c.a.b-mux21.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a-basic_blocks/a.c.a.c-mux21_generic.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a-basic_blocks/a.c.a.c-rca_n.vhd
+  a.b-DataPath.core/a.b-alu.core/a.c.a.b-PG.vhd
+  a.b-DataPath.core/a.b-alu.core/a.c.a.b-PG_general.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a.b-encoder.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a.b-functions.vhd
-  a.b-DataPath.core/a.b-alu.core/a.c.a.b-PG_general.vhd
-  a.b-DataPath.core/a.b-alu.core/a.c.a.b-PG.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a.c-G_general.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a.d-PG_network.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.a.e-sparse_tree_carry_gen.vhd
@@ -34,33 +38,31 @@ set SOURCE_FILES {
   a.b-DataPath.core/a.b-alu.core/a.c.e.b-logic_n.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.f-comparator.vhd
   a.b-DataPath.core/a.b-alu.core/a.c.g-zero_comp.vhd
-  a.b-DataPath.core/a.d.a-functions.vhd
   a.b-DataPath.core/a.d.b-ffd_async.vhd
   a.b-DataPath.core/a.d.c-reg_n.vhd
   a.b-DataPath.core/a.d.d-register_file.vhd
   a.b-DataPath.core/a.d.e-sign_extension.vhd
   a.b-DataPath.core/a.d.f-alu.vhd
   a.b-datapath.vhd
-  a-DLX.vhd
 }
-
-set ENTITY DLX
-set ARCH dlx_rtl
 
 sh rm -rf $LIBRARY
 sh mkdir -p $LIBRARY
 
 foreach file $SOURCE_FILES {
-  analyze -library $LIBRARY -format vhdl $SOURCE_DIRECTORY/$file
-}
+    analyze -library $LIBRARY -format vhdl $SOURCE_DIRECTORY/$file
+  }
+
+set ENTITY DLX
+set ARCH structural
 
 elaborate $ENTITY -architecture $ARCH -library DEFAULT -parameters "IR_SIZE = 32, PC_SIZE = 32"
 
-compile
-
-report_timing > $DEST_DIRECTORY/$ENTITY\_timing_nopt_max_path.rpt
-report_area   > $DEST_DIRECTORY/$ENTITY\_area_nopt_max_path.rpt
-report_power  > $DEST_DIRECTORY/$ENTITY\_power_nopt_max_path.rpt
-
-report_power -cell > $DEST_DIRECTORY/$ENTITY\_power_cell_nopt_max_path.rpt
-report_power -net  > $DEST_DIRECTORY/$ENTITY\_power_net_nopt_max_path.rpt
+# compile
+# 
+# report_timing > $DEST_DIRECTORY/$ENTITY\_timing_nopt_max_path.rpt
+# report_area   > $DEST_DIRECTORY/$ENTITY\_area_nopt_max_path.rpt
+# report_power  > $DEST_DIRECTORY/$ENTITY\_power_nopt_max_path.rpt
+# 
+# report_power -cell > $DEST_DIRECTORY/$ENTITY\_power_cell_nopt_max_path.rpt
+# report_power -net  > $DEST_DIRECTORY/$ENTITY\_power_net_nopt_max_path.rpt
